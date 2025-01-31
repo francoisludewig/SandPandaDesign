66182b815aa9db5b39a6f315b68d95aa06421361
p180004 <f.ludewig@helmo.be>
2025-01-31 15:39:29 +0100

    Json serialization suite : Cuboid, Lattice, Elbow & Setup. To be continue...

--- Repository/jsonserializer.cpp
+++ Repository/jsonserializer.cpp
@@ -126,3 +126,195 @@ std::shared_ptr<Cone> JsonSerializer::ConeFromJsonValue(Json::Value& jsonValue) 
     }
     return cone;
 }
+
+
+Json::Value JsonSerializer::ElbowToJsonValue(std::shared_ptr<Elbow>& elbow) {
+    Json::Value jsonElbow;
+    jsonElbow["xi"] = elbow->xi;
+    jsonElbow["yi"] = elbow->yi;
+    jsonElbow["zi"] = elbow->zi;
+    jsonElbow["xf"] = elbow->xf;
+    jsonElbow["yf"] = elbow->yf;
+    jsonElbow["zf"] = elbow->zf;
+    jsonElbow["cx"] = elbow->cx;
+    jsonElbow["cy"] = elbow->cy;
+    jsonElbow["cz"] = elbow->cz;
+    jsonElbow["vx"] = VelocityToJsonValue(elbow->vx);
+    jsonElbow["vy"] = VelocityToJsonValue(elbow->vy);
+    jsonElbow["vz"] = VelocityToJsonValue(elbow->vz);
+    jsonElbow["wx"] = VelocityToJsonValue(elbow->wx);
+    jsonElbow["wy"] = VelocityToJsonValue(elbow->wy);
+    jsonElbow["wz"] = VelocityToJsonValue(elbow->wz);
+    jsonElbow["radius"] = elbow->radius;
+    jsonElbow["Rc"] = elbow->Rc;
+    jsonElbow["alpha"] = elbow->alpha;
+    return jsonElbow;
+}
+
+std::shared_ptr<Elbow> JsonSerializer::ElbowFromJsonValue(Json::Value& jsonValue) {
+    auto elbow = std::make_shared<Elbow>();
+
+    elbow->xi = jsonValue["xi"].asDouble();
+    elbow->yi = jsonValue["yi"].asDouble();
+    elbow->zi = jsonValue["zi"].asDouble();
+    elbow->xf = jsonValue["xf"].asDouble();
+    elbow->yf = jsonValue["yf"].asDouble();
+    elbow->zf = jsonValue["zf"].asDouble();
+    elbow->cx = jsonValue["cx"].asDouble();
+    elbow->cy = jsonValue["cy"].asDouble();
+    elbow->cz = jsonValue["cz"].asDouble();
+
+    elbow->vx = VelocityFromJsonValue(jsonValue["vx"]);
+    elbow->vy = VelocityFromJsonValue(jsonValue["vy"]);
+    elbow->vz = VelocityFromJsonValue(jsonValue["vz"]);
+    elbow->wx = VelocityFromJsonValue(jsonValue["wx"]);
+    elbow->wy = VelocityFromJsonValue(jsonValue["wy"]);
+    elbow->wz = VelocityFromJsonValue(jsonValue["wz"]);
+
+    elbow->radius = jsonValue["radius"].asDouble();
+    elbow->Rc = jsonValue["Rc"].asDouble();
+    elbow->alpha = jsonValue["alpha"].asDouble();
+
+    elbow->base();
+
+    return elbow;
+}
+
+Json::Value JsonSerializer::CuboidToJsonValue(std::shared_ptr<Cuboid>& cuboid) {
+    auto asSolid = std::dynamic_pointer_cast<Solid>(cuboid);
+    Json::Value jsonCuboid = SolidToJsonValue(asSolid);
+    jsonCuboid["Lo"] = cuboid->Lo;
+    jsonCuboid["la"] = cuboid->la;
+    jsonCuboid["h"] = cuboid->h;
+    jsonCuboid["top"] = cuboid->top != nullptr;
+    jsonCuboid["bottom"] = cuboid->bottom != nullptr;
+    jsonCuboid["inte"] = cuboid->inte;
+    jsonCuboid["ped"] = cuboid->ped;
+    return jsonCuboid;
+}
+
+std::shared_ptr<Cuboid> JsonSerializer::CuboidFromJsonValue(Json::Value& jsonValue) {
+    auto cuboid = std::make_shared<Cuboid>();
+    auto asSolid = std::dynamic_pointer_cast<Solid>(cuboid);
+    cuboid->Lo = jsonValue["Lo"].asDouble();
+    cuboid->la = jsonValue["la"].asDouble();
+    cuboid->h = jsonValue["h"].asDouble();
+    cuboid->setTop(jsonValue["top"].asBool());
+    cuboid->setBottom(jsonValue["bottom"].asBool());
+    cuboid->inte = jsonValue["inte"].asBool();
+    cuboid->ped = jsonValue["ped"].asBool();
+    cuboid->updatePlans();
+    return cuboid;
+}
+
+Json::Value JsonSerializer::LatticeToJsonValue(std::shared_ptr<Lattice>& lattice) {
+    Json::Value jsonLattice;
+    jsonLattice["xmin"] = lattice->xmin;
+    jsonLattice["ymin"] = lattice->ymin;
+    jsonLattice["zmin"] = lattice->zmin;
+    jsonLattice["xmax"] = lattice->xmax;
+    jsonLattice["ymax"] = lattice->ymax;
+    jsonLattice["zmax"] = lattice->zmax;
+    jsonLattice["r0"] = lattice->r0;
+    jsonLattice["r1"] = lattice->r1;
+    jsonLattice["rho"] = lattice->rho;
+    jsonLattice["rdm"] = lattice->rdm;
+    jsonLattice["V"] = lattice->V;
+    jsonLattice["W"] = lattice->W;
+    jsonLattice["N"] = lattice->N;
+    return jsonLattice;
+}
+
+std::shared_ptr<Lattice> JsonSerializer::LatticeFromJsonValue(Json::Value& jsonValue) {
+    auto lattice = std::make_shared<Lattice>();
+    lattice->xmin = jsonValue["xmin"].asDouble();
+    lattice->ymin = jsonValue["ymin"].asDouble();
+    lattice->zmin = jsonValue["zmin"].asDouble();
+    lattice->xmax = jsonValue["xmax"].asDouble();
+    lattice->ymax = jsonValue["ymax"].asDouble();
+    lattice->zmax = jsonValue["zmax"].asDouble();
+
+    lattice->r0 = jsonValue["r0"].asDouble();
+    lattice->r1 = jsonValue["r1"].asDouble();
+    lattice->rho = jsonValue["rho"].asDouble();
+    lattice->V = jsonValue["V"].asDouble();
+    lattice->W = jsonValue["W"].asDouble();
+    lattice->N = jsonValue["N"].asInt();
+    lattice->rdm = jsonValue["rdm"].asBool();
+    lattice->Radius();
+    lattice->Position();
+    return lattice;
+}
+
+
+Json::Value JsonSerializer::SetupToJsonValue(std::shared_ptr<Setup>& setup) {
+    Json::Value jsonSetup;
+    jsonSetup["resitutionCoefficient"] = setup->resitutionCoefficient;
+    jsonSetup["normalStiffness"] = setup->normalStiffness;
+    jsonSetup["isTangentialDynamicModel"] = setup->isTangentialDynamicModel;
+    jsonSetup["staticFrictionCoefficient"] = setup->staticFrictionCoefficient;
+    jsonSetup["dynamicFrictionCoefficient"] = setup->dynamicFrictionCoefficient;
+
+    jsonSetup["gravityAcceleration"] = setup->gravityAcceleration;
+    jsonSetup["gravityX"] = setup->gravityX;
+    jsonSetup["gravityY"] = setup->gravityY;
+    jsonSetup["gravityZ"] = setup->gravityZ;
+
+    jsonSetup["gWxA0"] = setup->gWxA0;
+    jsonSetup["gWxA1"] = setup->gWxA1;
+    jsonSetup["gWxP"] = setup->gWxP;
+    jsonSetup["gWxW"] = setup->gWxW;
+    jsonSetup["gWyA0"] = setup->gWyA0;
+    jsonSetup["gWyA1"] = setup->gWyA1;
+    jsonSetup["gWyP"] = setup->gWyP;
+    jsonSetup["gWyW"] = setup->gWyW;
+    jsonSetup["gWzA0"] = setup->gWzA0;
+    jsonSetup["gWzA1"] = setup->gWzA1;
+    jsonSetup["gWzP"] = setup->gWzP;
+    jsonSetup["gWzW"] = setup->gWzW;
+
+    jsonSetup["duration"] = setup->duration;
+    jsonSetup["timeStep"] = setup->timeStep;
+    jsonSetup["captureTimeStep"] = setup->captureTimeStep;
+    jsonSetup["startingTime"] = setup->startingTime;
+
+    jsonSetup["Nsp"] = setup->Nsp;
+
+    return jsonSetup;
+}
+
+std::shared_ptr<Setup> JsonSerializer::SetupFromJsonValue(Json::Value& jsonValue, std::vector< std::shared_ptr<Lattice> > *lattices, std::shared_ptr<LinkedCells> linkedCells) {
+    auto setup = std::make_shared<Setup>(lattices, linkedCells);
+    setup->resitutionCoefficient = jsonValue["resitutionCoefficient"].asDouble();
+    setup->normalStiffness = jsonValue["normalStiffness"].asDouble();
+    setup->staticFrictionCoefficient = jsonValue["staticFrictionCoefficient"].asDouble();
+    setup->dynamicFrictionCoefficient = jsonValue["dynamicFrictionCoefficient"].asDouble();
+    setup->isTangentialDynamicModel = jsonValue["isTangentialDynamicModel"].asBool();
+
+    setup->gravityAcceleration = jsonValue["gravityAcceleration"].asDouble();
+    setup->gravityX = jsonValue["gravityX"].asDouble();
+    setup->gravityY = jsonValue["gravityY"].asDouble();
+    setup->gravityZ = jsonValue["gravityZ"].asDouble();
+
+    setup->gWxA0 = jsonValue["gWxA0"].asDouble();
+    setup->gWxA1 = jsonValue["gWxA1"].asDouble();
+    setup->gWxP = jsonValue["gWxP"].asDouble();
+    setup->gWxW = jsonValue["gWxW"].asDouble();
+    setup->gWyA0 = jsonValue["gWyA0"].asDouble();
+    setup->gWyA1 = jsonValue["gWyA1"].asDouble();
+    setup->gWyP = jsonValue["gWyP"].asDouble();
+    setup->gWyW = jsonValue["gWyW"].asDouble();
+    setup->gWzA0 = jsonValue["gWzA0"].asDouble();
+    setup->gWzA1 = jsonValue["gWzA1"].asDouble();
+    setup->gWzP = jsonValue["gWzP"].asDouble();
+    setup->gWzW = jsonValue["gWzW"].asDouble();
+
+    setup->duration = jsonValue["duration"].asDouble();
+    setup->timeStep = jsonValue["timeStep"].asDouble();
+    setup->captureTimeStep = jsonValue["captureTimeStep"].asDouble();
+    setup->startingTime = jsonValue["startingTime"].asDouble();
+
+    setup->Nsp = jsonValue["Nsp"].asDouble();
+
+    return setup;
+}
--- Repository/jsonserializer.h
+++ Repository/jsonserializer.h
@@ -5,6 +5,10 @@
 #include "Model/plan.h"
 #include "Model/disk.h"
 #include "Model/cone.h"
+#include "Model/elbow.h"
+#include "Model/cuboid.h"
+#include "Model/lattice.h"
+#include "Model/setup.h"
 
 class JsonSerializer
 {
@@ -17,9 +21,20 @@ public:
     static Json::Value DiskToJsonValue(std::shared_ptr<Disk>& disk);
     static std::shared_ptr<Disk> DiskFromJsonValue(Json::Value& jsonValue);
 
-
     static Json::Value ConeToJsonValue(std::shared_ptr<Cone>& cone);
     static std::shared_ptr<Cone> ConeFromJsonValue(Json::Value& jsonValue);
+
+    static Json::Value ElbowToJsonValue(std::shared_ptr<Elbow>& elbow);
+    static std::shared_ptr<Elbow> ElbowFromJsonValue(Json::Value& jsonValue);
+
+    static Json::Value CuboidToJsonValue(std::shared_ptr<Cuboid>& cuboid);
+    static std::shared_ptr<Cuboid> CuboidFromJsonValue(Json::Value& jsonValue);
+
+    static Json::Value LatticeToJsonValue(std::shared_ptr<Lattice>& lattice);
+    static std::shared_ptr<Lattice> LatticeFromJsonValue(Json::Value& jsonValue);
+
+    static Json::Value SetupToJsonValue(std::shared_ptr<Setup>& lattice);
+    static std::shared_ptr<Setup> SetupFromJsonValue(Json::Value& jsonValue, std::vector< std::shared_ptr<Lattice> > *lattices, std::shared_ptr<LinkedCells> linkedCells);
 
 private:
     static Json::Value SolidToJsonValue(std::shared_ptr<Solid>& solid);
